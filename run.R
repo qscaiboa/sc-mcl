c35aab27d2491880fec01d2625577992043b4dad
caipine <caipine@users.noreply.github.com>
2020-10-14 13:37:27 -0500

    Update run.R

--- run.R
+++ run.R
@@ -1,3 +1,5 @@
+
+
 library(Seurat)
 library(ggplot2)
 library(readr)
@@ -10,15 +12,42 @@ library(cowplot)
 library(BUSpaRse)
 library(tidyverse)
 library(DropletUtils)
-source("readPDX.R")
-dataDir  =  "/home/qcai1/Downloads/scCancer-master/Data/"
-figureDIR =  "/home/qcai1/Downloads/scCancer-master/Figures/"
-workDIR = "/home/qcai1/Downloads/scCancer-master/sc3MAP/10Xhm/" 
-workDIRh = "/home/qcai1/Downloads/scCancer-master/sc3MAP/10Xhuman/"
-workDIRm = "/home/qcai1/Downloads/scCancer-master/sc3MAP/10Xmouse/"
+source("readPDX2.R")
+
+dataDir  =  "/scratch/03988/qscai/hgmm/Data/"
+figureDIR =  "/scratch/03988/qscai/hgmm/Figures/"
+workDIR = "/scratch/03988/qscai/hgmm/sc3MAP/10Xhm/" 
+workDIRh = "/scratch/03988/qscai/hgmm/sc3MAP/10Xhuman/"
+workDIRm = "/scratch/03988/qscai/hgmm/sc3MAP/10Xmouse/"
+
+
 allSampleName = list.files(workDIR)
+allSampleName
 
-initialSampleName = "347795-Tumor"
+for (i in allSampleName) {
+print(i)
+readPDX(samplename=i, hmDIR=workDIR , humanDIR=workDIRh, mouseDIR=workDIRm,  figureDIR= figureDIR, dataDir= dataDir)
+list.files("./Data")
+}
 
-readPDX(samplename=initialSampleName, hmDIR=workDIR , humanDIR=workDIRh, mouseDIR=workDIRm,  figureDIR= figureDIR, dataDir= dataDir)
 
+
+readPDX_human <- function(samplename, humanDIR, dataDir) {
+  fileDIRh = paste(workDIRh, samplename, "/outs/filtered_feature_bc_matrix", sep="")
+  Data_tmp_h<-Read10X(data.dir = fileDIRh )
+  rownames(Data_tmp_h) <- toupper(rownames(Data_tmp_h))
+  ObjectH<- CreateSeuratObject(counts = Data_tmp_h, project = samplename, min.cells = 3, min.features=200)
+  ObjectH@meta.data$cellid <- rownames(ObjectH@meta.data)
+  ObjectH@meta.data$species <- "human" 
+  Object2 <- ObjectH
+
+  saveRDS(Object2, file = paste0(dataDir,samplename,".RDS"))
+  print(Object2)
+  }
+
+
+
+
+source("readPDX_human.R")
+readPDX_human(samplename="PBMC-1", humanDIR=workDIRh,   dataDir= dataDir)
+readPDX_human(samplename="PBMC-2", humanDIR=workDIRh,   dataDir= dataDir)
